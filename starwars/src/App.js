0bc48ba1d7f67282bc8833b91a21baaaedb255ba
Megan <mennis0145@gmail.com>
2019-09-13 08:38:20 +0100

    UseEffect hook implemented and returning expedted data from SWAPI

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
+import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [name, setName] = useState('');
+  const [height, setHeight] = useState('');
+  const [mass, setMass] = useState('');
+  const [yob, setYob] = useState('');
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people')
+      .then(response => {
+        response.data.results.map(person => {
+          console.log(person);
+        });
+        console.log(response.data.results);
+      })
+      .catch(error => {
+        debugger
+      });
+  }, );
 
   return (
     <div className="App">
